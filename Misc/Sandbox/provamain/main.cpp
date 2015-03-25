5900c8d52081e734f82e61e23d3de08cd7179ced
COMAU - C5GOpen workstation <c5gopen@c5gopen-APC910>
2015-03-25 14:29:06 +0100

    Extend a bit the C5G init functions + implement a test main reading positions from stdout

--- Misc/Sandbox/provamain/main.cpp
+++ Misc/Sandbox/provamain/main.cpp
@@ -3,9 +3,24 @@
 #include <Camera/DummyConsumer.h>
 #include <Camera/DummyProvider.h>
 
-int main(){
+int main(int argc, char** argv){
   using C5G::C5G;
-  C5G robot;
+  if(argc<3){
+    std::cerr << "Usage: " << argv[0] << " server profile\n";
+    std::cerr  << "Example: " << argv[0] << " 172.22.178.102 CNTRLC5G_2200102\n";
+    return -1;
+  }
+
+  std::string ip(argv[1]);
+  std::string profile(argv[2]);
+  C5G robot(ip, profile, false);
+  try{
+    robot.init();
+  }
+  catch(std::string ex){
+    std::cerr << ex << "\n";
+    return -2;
+  }
 
   boost::shared_ptr<Camera::ImageProvider> x(new Camera::DummyProvider());
   Camera::DummyConsumer img(x); 
