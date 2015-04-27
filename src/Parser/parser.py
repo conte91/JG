6510534f6d82a8e325def023bc2a209ec5b53249
Simone Baratta <Conte91@gmail.com>
2015-04-27 14:35:00 +0200

    Ohgodparser.pyisfinallymerged

--- src/Parser/parser.py
+++ src/Parser/parser.py
@@ -1,6 +1,9 @@
+import sys
+import string
 import json
 import cv2
-import libApcRobot
+import apcRobot
+import pyAPC
 
 if __name__ != "__main__":
     print("can't import me")
@@ -8,29 +11,40 @@ if __name__ != "__main__":
 with open("data/example.json") as goalsFile:
     goals = json.load(goalsFile)
 
-robotData = apcRobot.RobotData()
+robotData = apcRobot.RobotData.getInstance()
 
 binContents = goals['bin_contents']
 workOrders = goals['work_order']
 
-binNames = sorted(binContents.keys())
+binNumber=12
+binNames=map(lambda x:'bin_'+string.ascii_uppercase[x], range(0,binNumber))
+print binNames
 
+
+print binContents
 for position in range(len(binNames)):
-    row = position % 3
-    column = int(position/3)
+    column = position % 3
+    row = int(position/3)
     binName = binNames[position]
     itemNames = binContents[binName]
     for index in range(len(itemNames)):
         robotData.setBinItem(row,column,index,str(itemNames[index]))
 
 for position in range(len(binNames)):
-    row = position % 3
-    column = int(position/3)
+    column = position % 3
+    row = int(position/3)
     for index in range(5):
         itemName = robotData.getBinItem(row,column,index)
         print(row,column,index,itemName)
 
 for order in workOrders:
-    row = position % 3
-    column = int(position/3)
-    robotData.setWorkOrder(row,column,order)
+    position=binNames.index(order['bin'])
+    column = position % 3
+    row = int(position/3)
+    print(str(order['item']))
+    print("I'm flaggin"+str(order['item'])+" into row"+str(row)+" column"+str(column))
+
+
+    robotData.setWorkOrder(row,column,str(order['item']))
+
+pyAPC.theBigAPCPythonMain(len(sys.argv), sys.argv);
