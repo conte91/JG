019ad09c40b501881e5b99cc221ca56be3e583c3
Simone Baratta <Conte91@gmail.com>
2015-06-09 09:49:02 +0200

    Fix image cutter

--- include/Camera/Cutter.h
+++ include/Camera/Cutter.h
@@ -1,6 +1,8 @@
 #include <Camera/Image.h>
+#include <Camera/IndexTaker.h>
 
 namespace Camera{
   Image cutImage(const Image& input, int x0, int y0, int x1, int y1);
+  Image::Matrix getMask(const Image& input, const IndexTaker& taker);
   Image::Matrix getMask(const Image& input, int x0, int y0, int x1, int y1);
 }
--- src/Camera/Cutter.cpp
+++ src/Camera/Cutter.cpp
@@ -1,4 +1,8 @@
+#include <algorithm>
+#include <cstdlib>
+#include <Camera/Cutter.h>
 #include <Camera/Image.h>
+#include <Camera/IndexTaker.h>
 #include <cv.h>
 
 namespace Camera{
@@ -9,9 +13,15 @@ namespace Camera{
     return Image(depth, rgb);
   }
 
+  Image::Matrix getMask(const Image& input, const IndexTaker& taker){
+    return getMask(input, taker.getX1(), taker.getY1(), taker.getX2(), taker.getY2());
+  }
+
   Image::Matrix getMask(const Image& input, int x0, int y0, int x1, int y1){
+    using std::min;
+    using std::max;
     Image::Matrix theMask(cv::Mat::zeros(input.rgb.rows, input.rgb.cols, CV_8U));
-    theMask(cv::Rect(x0,y0,(x0-x1),(y0-y1))).setTo(255);
+    theMask(cv::Rect(min(x0, x1), min(y0, y1),abs(x0-x1),abs(y0-y1))).setTo(255);
     return theMask;
   }
 }
